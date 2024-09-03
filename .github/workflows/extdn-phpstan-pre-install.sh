6325cfd42e039ca2f928329eda36bda4c621802e
Jisse Reitsma <jisse@yireo.com>
2024-09-03 13:58:39 +0200

    Remove dev requirements, re-add bitexpert

--- .github/workflows/extdn-phpstan-pre-install.sh
+++ .github/workflows/extdn-phpstan-pre-install.sh
@@ -4,10 +4,10 @@ composer config prefer-stable false
 
 composer require yireo/magento2-integration-test-helper --no-update
 
-#composer config --no-plugins allow-plugins true
+composer config --no-plugins allow-plugins true
 
-#composer require --dev phpstan/extension-installer --no-update
-#composer require --dev bitexpert/phpstan-magento --no-update
+composer require --dev phpstan/extension-installer --no-update
+composer require --dev bitexpert/phpstan-magento --no-update
 
 composer require yireo/magento2-replace-bundled:^4.0 --no-update
 composer require yireo/magento2-replace-inventory:^4.0 --no-update
